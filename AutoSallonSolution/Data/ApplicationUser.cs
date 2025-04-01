8a0d1e9d94f938e9e70f31b5533fed080a620de7
meriton <137052060+meriton41@users.noreply.github.com>
2025-04-02 00:09:32 +0200

    Gjenerimi i Access Token dhe RefreshToken,krijimi i register dhe login formes ne backend.

---
+++ AutoSallonSolution/Data/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace AutoSallonSolution.Data
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Name { get; set; }
+    }
+}
